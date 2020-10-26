a2a8d1e9ed3972540602e51df57867247d76f14e
Ansoni <antoniogviegas@hotmail.com>
2020-10-26 22:35:11 +0100

    Refactor constructors to improve semantic

--- src/ifc-utils/ifc-constructor.js
+++ src/ifc-utils/ifc-constructor.js
@@ -1,17 +1,29 @@
 import { regexp } from "./items-parser";
 
 function baseConstructor(caller, ifcLine, classToConstruct) {
-  return regexp.defaultValue.test(ifcLine)
-    ? caller.parser.getDefaultValue(ifcLine)
+  return containsDefaultValue(ifcLine)
+    ? getDefaultValue(caller, ifcLine)
     : new classToConstruct(caller.finder, ifcLine);
 }
 
 function baseMultiConstructor(caller, classToConstruct) {
   return caller.isEmptySet()
     ? caller.extractEmptySet()
-    : caller.extractIdSet().map((e) => {
-        return new classToConstruct(caller.finder, e);
-      });
+    : instantiateClasses(caller, classToConstruct);
+}
+
+function containsDefaultValue(ifcLine) {
+  return regexp.defaultValue.test(ifcLine);
+}
+
+function getDefaultValue(caller, ifcLine) {
+  return caller.parser.getDefaultValue(ifcLine);
+}
+
+function instantiateClasses(caller, classToConstruct) {
+  return caller.extractIdSet().map((e) => {
+    return new classToConstruct(caller.finder, e);
+  });
 }
 
 export { baseConstructor, baseMultiConstructor };
