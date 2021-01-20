1c5f0cdb352ca0443d351b8190c7151d28b0ccd1
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-01-20 20:27:41 +0100

    chore: add typechecker

--- typecheck.js
+++ typecheck.js
@@ -1,14 +1,21 @@
 const { TypeChecker } = require('esbuild-helpers');
 
-// I would use splitview and have this in own view
+const TEST = process.argv[2] === 'test';
+const TEST_WATCH = process.argv[2] === 'test' && process.argv[3] === 'watch';
 
-const checker_client = TypeChecker({
-  basePath: './src',
-  name: 'checker_client',
-  tsConfig: './tsconfig.json',
-  print_summary: true,
-  print_runtime: true
-});
+if (TEST) {
+  const checker_client = TypeChecker({
+    basePath: './src',
+    name: 'checker_client',
+    tsConfig: './tsconfig.json',
+    print_summary: true,
+    print_runtime: true
+  });
 
-checker_client.printSettings();
-checker_client.inspectAndPrint();
+  checker_client.printSettings();
+  checker_client.inspectAndPrint();
+
+  if (TEST_WATCH) {
+    checker_client.worker_watch('./');
+  }
+}
