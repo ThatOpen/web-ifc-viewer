e757b7f269fdd53c57f51bc1a9ba54816c07e2f5
Ansoni <antoniogviegas@hotmail.com>
2021-01-22 18:50:30 +0100

    Start implementing ellipse curves

--- src/ifc-to-three.js/geometry-generator/curves/trimmed-curve.js
+++ src/ifc-to-three.js/geometry-generator/curves/trimmed-curve.js
@@ -13,7 +13,8 @@ function mapTrimmedCurveAsShape(shape, curve) {
 }
 
 const trimmedCurvesMap = {
-  [t.IfcCircle]: { shape: mapTrimmedCircleShape, line: mapTrimmedCircleLine }
+  [t.IfcCircle]: { shape: mapTrimmedCircleShape, line: mapTrimmedCircleLine },
+  [t.IfcEllipse]: { shape: mapTrimmedEllipseShape, line: mapTrimmedCircleLine }
 };
 
 function mapTrimmedCircleLine(curve) {
@@ -21,6 +22,10 @@ function mapTrimmedCircleLine(curve) {
   const circleCurve = new THREE.EllipseCurve(x, y, radius, radius, trims[0], trims[1], false, 0);
   const points = circleCurve.getPoints(50).map((point) => [point.x, point.y]);
   return createLine(points);
+}
+
+function mapTrimmedEllipseLine(curve){
+  //TODO
 }
 
 //Three.js draw shapes continuously
@@ -37,28 +42,42 @@ function mapTrimmedCircleShape(shape, curve) {
     : shape.absarc(x, y, radius, trims[1], trims[0], true);
 }
 
-function getDistancesToNextPoints(currentPoint, ends) {
-  return [
-    getDistanceBetweenPoints(currentPoint, ends[0]),
-    getDistanceBetweenPoints(currentPoint, ends[1])
-  ];
+function mapTrimmedEllipseShape(shape, curve) {
+  const { x, y, a, b, trims, ends } = getEllipseInfo(curve);
+  const currentPoint = [shape.currentPoint.x, shape.currentPoint.y];
+  const distancesToNextPoints = getDistancesToNextPoints(currentPoint, ends);
+  distancesToNextPoints[0] < distancesToNextPoints[1]
+    ? shape.absellipse(x, y, a, b, trims[0], trims[1], false)
+    : shape.absellipse(x, y, a, b, trims[1], trims[0], true);
 }
 
 function getCircleInfo(curve) {
-  const location = curve[n.basisCurve][n.position][n.location][n.coordinates];
   const radius = curve[n.basisCurve][n.radius];
-  const x = location[0];
-  const y = location[1];
-  const trims = getCircleTrims(curve);
+  const { x, y } = getCurveLocation(curve);
+  const trims = getCurveTrims(curve);
   const ends = getCircleEnds(x, y, radius, trims);
   return { x, y, radius, trims, ends };
 }
 
-function getCircleTrims(curve) {
-  return [getCircleTrim(curve, [n.trim1]), getCircleTrim(curve, [n.trim2])];
+function getEllipseInfo(curve) {
+  const { x, y } = getCurveLocation(curve);
+  const a = curve[n.basisCurve][n.semiAxis1];
+  const b = curve[n.basisCurve][n.semiAxis2];
+  const trims = getCurveTrims(curve);
+  const ends = getEllipseEnds(x, y, a, b, trims);
+  return { x, y, a, b, trims, ends };
 }
 
-function getCircleTrim(curve, trim) {
+function getCurveLocation(curve) {
+  const loc = curve[n.basisCurve][n.position][n.location][n.coordinates];
+  return { x: loc[0], y: loc[1] };
+}
+
+function getCurveTrims(curve) {
+  return [getCurveTrim(curve, [n.trim1]), getCurveTrim(curve, [n.trim2])];
+}
+
+function getCurveTrim(curve, trim) {
   const rotation = curve[n.basisCurve][n.position][n.refDirection][n.dirRatios];
   const offsetAngle = Math.acos(rotation[0]);
   return (curve[trim][0][i.value] * Math.PI) / 180 + offsetAngle;
@@ -70,6 +89,28 @@ function getCircleEnds(x, y, radius, trims) {
 
 function getCircleEnd(x, y, radius, angle) {
   return [Math.cos(angle) * radius + x, Math.sin(angle) * radius + y];
+}
+
+function getEllipseEnds(x, y, a, b, trims) {
+  return [
+    getEllipseEnd(x, y, a, b, trims[0]),
+    getEllipseEnd(x, y, a, b, trims[1])
+  ];
+}
+
+function getEllipseEnd(x, y, a, b, trim) {
+  const radiansAngle = trim * Math.PI / 180;
+  const factor = trim > 3*Math.PI/2 || trim < Math.PI/2 ? -1 : 1;
+  const endX = (a * b) / Math.sqrt(b*b + a*a*Math.tan(radiansAngle)) * factor;
+  const endY = x * Math.tan(radiansAngle);
+  return {endX, endY}
+}
+
+function getDistancesToNextPoints(currentPoint, ends) {
+  return [
+    getDistanceBetweenPoints(currentPoint, ends[0]),
+    getDistanceBetweenPoints(currentPoint, ends[1])
+  ];
 }
 
 function getDistanceBetweenPoints(point1, point2) {
--- src/ifc-to-three.js/geometry-generator/ifc-geometry/ifc-annotation.js
+++ src/ifc-to-three.js/geometry-generator/ifc-geometry/ifc-annotation.js
@@ -2,7 +2,6 @@ import { namedProps as n } from '../../../utils/global-constants.js';
 
 function mapAnnotation(shape) {
     //TODO
-  console.log("TODO:", shape);
   return new THREE.Object3D();
 }
 
