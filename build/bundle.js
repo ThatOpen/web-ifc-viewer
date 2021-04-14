7c64b609734bacde2ab7faaa7bc2d79a102d7bb0
Ansoni <antoniogviegas@hotmail.com>
2021-04-14 17:27:52 +0200

    Hide IFC properties menu (until properties API is finished)

--- build/bundle.js
+++ build/bundle.js
@@ -42281,48 +42281,6 @@ function loadIfc(event) {
   });
 }
 
-function setupIfcPropertiesMenu() {
-  const sideMenuRight = document.getElementById('side-menu-right');
-  const propsMenu = document.createElement('div');
-  propsMenu.classList.add('side-sub-menu');
-  sideMenuRight.appendChild(propsMenu);
-
-  const propsMenuTitle = document.createElement('div');
-propsMenuTitle.classList.add("text", "text-title");
-  propsMenuTitle.appendChild(document.createTextNode("IFC Properties"));
-
-  const propsMenuContent = document.createElement('div');
-  propsMenuContent.setAttribute("id", "props-menu-content");
-  
-  propsMenu.appendChild(propsMenuTitle);
-  propsMenu.appendChild(propsMenuContent);
-
-  addPropertiesToMenu(["kdfjaldfkjaaaaa aaaaaa aaaaaaaaaa aaaaaaaaaa aaaaaaaaa aaaaaaaaaaa aaaaala", "fakdfalja", "1234", "kkkkkkkk"]);
-
-}
-
-function updatePropertiesMenu(props){
-    clearPropertiesFromMenu();
-    addPropertiesToMenu(props);
-}
-
-function addPropertiesToMenu(props) {
-  props.forEach((prop) => createPropertyEntry(prop));
-}
-
-function clearPropertiesFromMenu() {
-  const propsMenu = document.getElementById('props-menu-content');
-  propsMenu.innerHTML = '';
-}
-
-function createPropertyEntry(info) {
-  const propertyLine = document.createElement('div');
-  propertyLine.classList.add('text');
-  propertyLine.appendChild(document.createTextNode(`${info}`));
-  const propsMenu = document.getElementById("props-menu-content");
-  propsMenu.appendChild(propertyLine);
-}
-
 //source: https://threejsfundamentals.org/threejs/lessons/threejs-picking.html
 
 //TODO: Use GPU picking to work toguether with clipping planes
@@ -42365,13 +42323,13 @@ function pick(event) {
     pickedItem.material = selectedMaterial;
     pickedItem.isSelected = true;
 
-    const props = ifcLoader.getPropertiesById(pickedItem.expressID);
-    updatePropertiesMenu(props.arguments);
+    ifcLoader.getPropertiesById(pickedItem.expressID);
+    // updatePropertiesMenu(props.arguments);
   }
 }
 
 setupThreeScene();
-setupIfcPropertiesMenu();
+// setupIfcPropertiesMenu();
 setupIfcReader();
 setupEdgesDisplay();
 setupScenePicking();
