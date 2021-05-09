722b9a3011156fd8bd48f17995026b082b09a9b5
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-05-09 20:47:43 +0200

    feat: set camera & control to focus on imported model

--- src/files/file-loader.js
+++ src/files/file-loader.js
@@ -1,5 +1,6 @@
-import { scene, ifcLoader } from '../scene/scene';
+import { scene, ifcLoader, camera, cameraControls } from '../scene/scene';
 import { createSideMenuButton } from '../gui/gui-creator';
+import * as THREE from 'three';
 
 let loadedObject;
 
@@ -21,11 +22,39 @@ function createInputElement() {
   return inputElement;
 }
 
+function fitModelToFrame() {
+  const box = new THREE.Box3().setFromObject(scene.children[scene.children.length - 1]);
+  const boxSize = box.getSize(new THREE.Vector3()).length();
+  const boxCenter = box.getCenter(new THREE.Vector3());
+ 
+
+  const halfSizeToFitOnScreen = boxSize * 0.5;
+  const halfFovY = THREE.MathUtils.degToRad(camera.fov * 0.5);
+  const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+
+  const direction = new THREE.Vector3()
+    .subVectors(camera.position, boxCenter)
+    .multiply(new THREE.Vector3(1, 0, 1))
+    .normalize();
+
+  camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+  camera.near = 0.001;
+  camera.far = boxSize * 200;
+  camera.updateProjectionMatrix();
+  camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+
+  // set target to newest loaded model
+  cameraControls.target.copy(boxCenter);
+  cameraControls.update();
+}
+
 function loadIfc(event) {
   var ifcURL = URL.createObjectURL(event.target.files[0]);
   ifcLoader.load(ifcURL, (geometry) => {
     loadedObject = geometry;
     loadedObject.isIFC = true;
     scene.add(loadedObject);
+
+    fitModelToFrame();
   });
 }
