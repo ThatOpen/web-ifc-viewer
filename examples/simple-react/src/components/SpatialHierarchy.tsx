104978b361aab71804d1d58249c3ac42e28c6b2d
Harry Collin <harry.archie.collin@gmail.com>
2021-06-13 23:32:43 +0100

    Update SpatialHierarchy.tsx

--- examples/simple-react/src/components/SpatialHierarchy.tsx
+++ examples/simple-react/src/components/SpatialHierarchy.tsx
@@ -1,5 +1,5 @@
 import { Viewer } from 'web-ifc-viewer';
-import React, { FunctionComponent, useCallback, useMemo, useState } from 'react';
+import React, { FunctionComponent, useCallback, useEffect, useMemo, useState } from 'react';
 import { StyledTreeItem, StyledTreeView } from './StyledTreeView';
 import { Box, IconButton, Tooltip, Typography } from '@material-ui/core';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
@@ -14,55 +14,59 @@ export const SpatialHierarchy: FunctionComponent<SpatialHierarchyProps> = (props
 
     const { viewer } = props
 
-    console.log(viewer.ifcLoader.getSpatialStructure())
-
     return (
         <StyledTreeView>
             {viewer.ifcLoader.getSpatialStructure().hasSpatialChildren.map((element: any, index: number) => (
-                  <TreeNode key={index} viewer={viewer} element={element} />
+                  <TreeNode key={index} viewer={viewer} element={element}  overrideVisibility={null} />
             ))}
         </StyledTreeView>
     )
 }
 
-
 interface TreeNodeProps {
     viewer: Viewer,
-    element: any
+    element: any,
+    overrideVisibility: boolean | null
 }
 
 const TreeNode: FunctionComponent<TreeNodeProps> = (props) => {
 
-    const { viewer, element } = props;
+    const { viewer, element, overrideVisibility } = props;
 
     const [visible, setVisible] = useState(true)
 
-    /*// @ts-ignore */
     const name = element.Name.value;
-    // @ts-ignore
     const id = element.GlobalId.value;
-
     const expressId = element.expressID;
-
-    const toggleVisibility = useCallback (
-      (event: React.MouseEvent<HTMLAnchorElement> | React.MouseEvent<HTMLButtonElement>) => {
-          event.stopPropagation();
-
-          const v = !visible;
-          setVisible(v);
-
+    
+    const setVisibility = useCallback(
+      (visible: boolean) => {
           const mesh = viewer.ifc_objects[0] as Mesh;
           const display: Display = {
               r: 0,
               g: 0,
               b: 0,
-              a: v ? 1 : 0,
-              h: v ? 0 : 1,
+              a: visible ? 1 : 0,
+              h: visible ? 0 : 1,
           }
-          viewer.ifcLoader.setItemsDisplay([expressId], mesh, display, viewer.scene)
-
-      }, [expressId, viewer.ifcLoader, viewer.ifc_objects, viewer.scene, visible]
+          viewer.ifcLoader.setItemsDisplay([expressId], mesh, display, viewer.scene);
+          setVisible(visible);
+      },
+      [expressId, viewer.ifcLoader, viewer.ifc_objects, viewer.scene]
     );
+
+    const handleClickToggleVisibility = useCallback (
+      (event: React.MouseEvent<HTMLAnchorElement> | React.MouseEvent<HTMLButtonElement>) => {
+          event.stopPropagation();
+          setVisibility(!visible);
+      }, [setVisibility, visible]
+    );
+
+    useEffect(() => {
+        if(overrideVisibility !== null){
+            setVisibility(overrideVisibility);
+        }
+    }, [overrideVisibility, setVisibility]);
 
     const label = useMemo(() => (
       <Box display="flex" p={0.2} alignItems="center" justifyContent='space-between' >
@@ -70,33 +74,31 @@ const TreeNode: FunctionComponent<TreeNodeProps> = (props) => {
               <Typography variant="caption" noWrap color={visible ? "inherit" : "textSecondary" }>{name}</Typography>
           </Tooltip>
           <Box display="flex" flexGrow={1} justifyContent="flex-end" ml={0.5}>
-              <IconButton size='small' onClick={toggleVisibility}>
+              <IconButton size='small' onClick={handleClickToggleVisibility}>
                   {visible ? <Visibility style={{ fontSize: "1rem" }}/> : <VisibilityOff  style={{ fontSize: "1rem" }} /> }
               </IconButton>
           </Box>
       </Box>
-    ), [name, toggleVisibility, visible]);
+    ), [name, handleClickToggleVisibility, visible]);
 
     const childNodes = useMemo(() => {
         const childNodes: JSX.Element[] = []
 
         element?.hasSpatialChildren?.forEach((child: any) => {
             childNodes.push(
-              // @ts-ignore
-              <TreeNode key={child.GlobalId.value}  viewer={viewer} element={child}/>
+              <TreeNode key={child.GlobalId.value}  viewer={viewer} element={child} overrideVisibility={visible} />
             )
         })
 
         element?.hasChildren?.forEach((child: any) => {
             const data = viewer.ifcLoader.getItemProperties(child, false) as any;
             childNodes.push (
-              // @ts-ignore
-              <TreeNode key={data.GlobalId.value}  viewer={viewer} element={data}/>
+              <TreeNode key={data.GlobalId.value}  viewer={viewer} element={data} overrideVisibility={visible} />
             )
         })
 
         return childNodes;
-    }, [element.hasChildren, element.hasSpatialChildren, viewer]);
+    }, [element?.hasChildren, element?.hasSpatialChildren, viewer, visible]);
 
     return (
       <StyledTreeItem nodeId={id} label={label}>
