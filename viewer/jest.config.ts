82eb7245bebd5b1db76d6283966b462749887c31
GP4cK <gautier.bayzelon@gmail.com>
2021-06-17 23:17:28 +0800

    🔧 setup ts-jest to work with ESM modules

---
+++ viewer/jest.config.ts
@@ -0,0 +1,18 @@
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
+  },
+  // These node_modules need to be transpiled
+  // https://stackoverflow.com/a/63390125/3466729
+  transformIgnorePatterns: ['node_modules/(?!(web-ifc-three|web-ifc|three))'],
+};
+export default config;
