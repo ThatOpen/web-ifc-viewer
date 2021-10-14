69620177ecb66da3b0d1258f53929efd11e0c4fa
Antonio <antoniogviegas@hotmail.com>
2021-10-14 13:09:56 +0200

    Update dependencies and examples

--- example/main.js
+++ example/main.js
@@ -13,9 +13,7 @@ viewer.IFC.loader.ifcManager.applyWebIfcConfig({
   COORDINATE_TO_ORIGIN: true,
   USE_FAST_BOOLS: false
 });
-// viewer.IFC.loader.ifcManager.useJSONData();
 viewer.IFC.loader.ifcManager.useWebWorkers(true, 'files/IFCWorker.js');
-// viewer.IFC.loader.ifcManager.loadJsonDataFromWorker(0, '01.json');
 
 //Setup loader
 const loadIfc = async (event) => {
@@ -34,64 +32,23 @@ const loadIfc = async (event) => {
   overlay.classList.add("hidden")
 };
 
-
 const inputElement = document.createElement('input');
 inputElement.setAttribute('type', 'file');
 inputElement.classList.add('hidden');
 inputElement.addEventListener('change', loadIfc, false);
 document.body.appendChild(inputElement);
 
-viewer.dimensions.active = true;
-viewer.dimensions.previewActive = true;
-
 const handleKeyDown = (event) => {
   if (event.code === 'Delete') {
     viewer.removeClippingPlane();
-    viewer.dimensions.delete();
-    viewer.context.ifcCamera.unlock();
-  }
-  if (event.code === 'Space') {
-    viewer.context.ifcCamera.setNavigationMode(NavigationModes.FirstPerson);
-    viewer.IFC.unPrepickIfcItems();
-    window.onmousemove = null;
-  }
-  if (event.code === 'KeyH') {
-    viewer.context.ifcCamera.goToHomeView();
-  }
-  if (event.code === 'KeyE') {
-    viewer.edges.toggleEdges()
-  }
-  if (event.code === 'KeyD') {
-    // viewer.dimensions.create();
-    viewer.context.ifcCamera.setNavigationMode(NavigationModes.FirstPerson);
-  }
-  if (event.code === 'Escape') {
-    window.onmousemove = viewer.IFC.prePickIfcItem;
-  }
-  if (event.code === "KeyP") {
-    viewer.context.ifcCamera.projection = CameraProjections.Perspective;
-  }
-  if (event.code === "KeyO") {
-    viewer.context.ifcCamera.projection = CameraProjections.Orthographic;
   }
 };
 
-// window.onmousemove = viewer.IFC.prePickIfcItem;
+window.onmousemove = viewer.IFC.prePickIfcItem;
 window.onkeydown = handleKeyDown;
 window.ondblclick = async () => {
   viewer.clipper.createPlane();
-  // const result = await viewer.IFC.pickIfcItem(true);
-  // if(result) {
-  //   // const props = await viewer.IFC.getProperties(result.modelID, result.id, true);
-  //   const all = await viewer.IFC.getAllItemsOfType(result.modelID, IFCWALLSTANDARDCASE, false);
-  //   console.log(all);
-  // }
 }
-
-// viewer.IFC.applyWebIfcConfig({
-//   COORDINATE_TO_ORIGIN: true,
-//   USE_FAST_BOOLS: true
-// });
 
 //Setup UI
 const loadButton = createSideMenuButton('./resources/folder-icon.svg');
